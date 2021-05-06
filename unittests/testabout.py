227508251a7a6658281ce57e762cae120c2ecd59
jitsuineccles <paul.hewlett@jitsuin.com>
2021-05-06 11:23:26 +0100

    100% test coverage with bad pylint (#15)
    * 100% test coverage with bad pylint
    
    * Run QC on any push or pull_request event
    
    * Create about.py during QC action
    
    * Correct bad pylint - QC should pass
    
    * black does not rase error if fie is reforatted
    
    * Fail QC if any files need formatting by black
    
    * File reformatted by black - QC should pass
    
    * Coverage fail_under set - QC should fail
    
    * Coverage is 100% - QC should pass

---
+++ unittests/testabout.py
@@ -0,0 +1,27 @@
+"""
+Test archivist
+"""
+
+# pylint: disable=attribute-defined-outside-init
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
+
+from unittest import TestCase
+
+from archivist import about
+
+
+class TestAbout(TestCase):
+    """
+    Test about
+    """
+
+    def test_about(self):
+        """
+        Test about
+        """
+        self.assertGreater(
+            len(about.__version__),
+            0,
+            msg="about version should be populated",
+        )
