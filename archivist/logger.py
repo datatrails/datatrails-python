0249bf33faff827908108d0e00a7593e052ec8aa
Paul Hewlett <phewlett76@gmail.com>
2021-09-03 14:42:02 +0100

    Add some common code from archivist-samples
    Problem:
    Unclear how to use from CLI.
    
    Solution:
    Added convenience code for argparse and setting up a common Archivist
    endpoint.
    
    Signed-off-by: Paul Hewlett <phewlett76@gmail.com>

---
+++ archivist/logger.py
@@ -0,0 +1,38 @@
+"""Creates the root logger as a simple console streamer. Follows the Django-style
+   logging configuration where the logging system forms a hierarchy of loggers
+   all potentially independently configurable.
+
+   This model allows controlling loggers that are part of the dependency list. For
+   example setting a debug logging level will show debug output from the underlying
+   urllib3 package.
+
+   URL: https://docs.djangoproject.com/en/3.2/topics/logging/
+
+   This code is not used directly by this package...
+"""
+
+# pylint:  disable=missing-docstring
+
+import logging
+import logging.config
+
+# root logger for all code
+LOGGER = logging.getLogger()
+
+
+def set_logger(level):
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "handlers": {
+                "console": {
+                    "class": "logging.StreamHandler",
+                },
+            },
+            "root": {
+                "handlers": ["console"],
+                "level": level,
+            },
+        }
+    )
