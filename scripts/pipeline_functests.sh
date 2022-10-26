d92234dd02f6e1ec02b1c56eaf557d3a93b37f57
serhiy <serhiy.pikho@jitsuin.com>
2022-10-26 10:39:19 +0100

    force exit 0 on the pipeline function
    problem:
        - The non 0 exit code from test Failures looks like script failures in the internal CD
        - The test subjects count test is flakey due to other tests interfering with it
    Solution:
        - Set the exit code of pipeline_functests.sh to be 0
        - fix the flakey test
    signed-off-by:
        serhiy <serhiy1@live.co.uk>

--- scripts/pipeline_functests.sh
+++ scripts/pipeline_functests.sh
@@ -6,3 +6,4 @@ rm -rf functest-results
 mkdir -p functest-results
 export PYTHONWARNINGS="ignore:Unverified HTTPS request"
 python -m xmlrunner discover -v -p exec*.py -s functests -o ./functest-results/
+exit 0
