d6f377587cbaa680958daecb0e226b2f4d0bf75b
eccles <eccles@users.noreply.github.com>
2022-11-01 12:12:56 +0000

    deploy: cebba7e9a57feca85b5ebcce5876ec5acf2d994f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.17.3-0-g44b51c9',
+    VERSION: 'v0.17.4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
